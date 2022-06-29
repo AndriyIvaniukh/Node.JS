c04a84c2633c6b3e64a8d96164074bbdcb9577be
AndriyIvaniukh <andriyivaniukh@gmail.com>
2022-06-30 00:17:58 +0300

    lesson5 next step

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,11 @@
 const {userService, passwordService} = require("../service");
+const {userPresenter} = require("../presenters/user.presenter");
 
 async function getAll(req, res, next) {
     try {
-        const users = await userService.findUsers();
-        res.json(users);
+        const users = await userService.findUsers().exec();
+        const usersForResponse = users.map(user => userPresenter(user));
+        res.json(usersForResponse);
     } catch (e) {
         next(e);
     }
@@ -13,7 +15,8 @@ async function getAll(req, res, next) {
 async function getByID(req, res, next) {
     try {
         const {user} = req;
-        res.json(user);
+        const userForResponse = userPresenter(user);
+        res.json(userForResponse);
     } catch (e) {
         next(e);
     }
@@ -23,7 +26,8 @@ async function updateUserByID(req, res, next) {
     try {
         const {id} = req.params;
         const updatedUser = await userService.updateOneUser({_id: id}, req.body);
-        res.status(201).send(updatedUser);
+        const userForResponse = userPresenter(updatedUser);
+        res.status(201).send(userForResponse);
     } catch (e) {
         next(e);
     }
@@ -33,7 +37,8 @@ async function createUser(req, res, next) {
     try {
         const hashedPassword = await passwordService.hashPassword(req.body.password);
         const newUser = await userService.createUser({...req.body, password: hashedPassword})
-        res.status(201).json(newUser);
+        const userForResponse = userPresenter(newUser);
+        res.status(201).json(userForResponse);
     } catch (e) {
         next(e);
     }
